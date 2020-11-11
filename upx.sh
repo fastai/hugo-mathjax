d4a53516c10afa4c05595a058e2253925fc318c8
Jeremy Howard <j@fast.ai>
2020-11-11 13:53:45 -0800

    Update upx.sh

--- upx.sh
+++ upx.sh
@@ -8,4 +8,4 @@ chmod 700 ~/.ssh
 rsync -e "ssh -o StrictHostKeyChecking=no" -az jeremyphoward@ps625762.dreamhostps.com:files.fast.ai/hugo-full/$DEST_OS/hugo ./ 
 upx hugo
 
-rsync -e "ssh -o StrictHostKeyChecking=no" -az hugo jeremyphoward@ps625762.dreamhostps.com:files.fast.ai/hugo-full/$DEST_OS/
+rsync -e "ssh -o StrictHostKeyChecking=no" -az hugo jeremyphoward@ps625762.dreamhostps.com:files.fast.ai/hugo/$DEST_OS/
