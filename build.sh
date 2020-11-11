741440ca42b2aa5f6eb6ad68020414e3d590b5b7
Jeremy Howard <j@fast.ai>
2020-11-11 11:51:38 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
+case "$OSTYPE" in
+  darwin*)
+    brew install upx rsync
+    DEST_OS=macos
+    ;;
+  linux*)
+    apt-get update
+    apt-get install -y rsync
+    DEST_OS=linux
+    ;;
+  *)
+    echo "unknown: $OSTYPE"
+    ;;
+esac
+
 git clone https://github.com/gohugoio/hugo.git
 cd hugo
 latesttag=$(git describe --tags)
 git checkout ${latesttag}
 patch -p1 < ../hugo.patch
 go build --tags extended
-[[ "$OSTYPE" == "darwin"* ]] && brew install upx
 upx hugo
-pwd
+
+mkdir -p ~/.ssh
+echo "$SSH_KEY" > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+chmod 700 ~/.ssh
+rsync -e "ssh -o StrictHostKeyChecking=no" -az hugo jeremyphoward@ps625762.dreamhostps.com:files.fast.ai/hugo/$DEST_OS/
