38d2f56cca5c850e000389971e95d168212b3c4e
Jeremy Howard <j@fast.ai>
2020-11-11 17:57:04 -0800

    Update build.py

--- build.py
+++ build.py
@@ -1,3 +1,12 @@
-from fastcore.all import *
-import fastcore
-print(fastcore.__version__)
+from fastcore.utils import *
+from urllib.request import urlopen
+import tarfile
+
+user,repo = 'gohugoio','hugo'
+rels = urljson(f'https://api.github.com/repos/{user}/{repo}/releases/latest')
+dest = Path('hugo-download')
+with urlopen(urlwrap(rels['tarball_url'])) as u:
+    tarfile.open(mode='r:gz', fileobj=u).extractall(dest)
+
+dest.ls()[0].rename('hugo')
+dest.rmdir()
