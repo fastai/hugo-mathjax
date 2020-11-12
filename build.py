64f8cb0b85f27ebc645ad28cb1a10b73de9abfea
Jeremy Howard <j@fast.ai>
2020-11-11 19:38:46 -0800

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 from fastcore.utils import *
+import os
 
 repo = 'hugo'
 rels = urljson(f'https://api.github.com/repos/gohugoio/{repo}/releases/latest')
 with urlopen(rels['tarball_url']) as f: untar_dir(f, repo)
-run(f'patch -l -p1 -d {repo} -i ../hugo.patch')
+os.chdir(repo)
+run(f'patch -l -p1 -i ../hugo.patch')
 run('go build --tags extended')
