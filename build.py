07b6f7bf02ec4c210af10b988fc9c54465bcdef8
Jeremy Howard <j@fast.ai>
2020-11-11 19:27:42 -0800

    Update build.py

--- build.py
+++ build.py
@@ -1,15 +1,7 @@
 from fastcore.utils import *
-from urllib.request import urlopen
-import tarfile
 
-user,repo = 'gohugoio','hugo'
-rels = urljson(f'https://api.github.com/repos/{user}/{repo}/releases/latest')
-dest = Path(f'{repo}-download')
-with urlopen(urlwrap(rels['tarball_url'])) as u:
-    tarfile.open(mode='r:gz', fileobj=u).extractall(dest)
-
-dest.ls()[0].rename(repo)
-dest.rmdir()
-
+repo = 'hugo'
+rels = urljson(f'https://api.github.com/repos/gohugoio/{repo}/releases/latest')
+with urlopen(rels['tarball_url']) as f: untar_dir(f, repo)
 run(f'patch -l -p1 -d {repo} -i ../hugo.patch')
 run('go build --tags extended')
