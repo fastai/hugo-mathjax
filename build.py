4ed8357af731a2eb276d454648cdd715a7b24f5e
Jeremy Howard <j@fast.ai>
2020-11-11 18:55:40 -0800

    Update build.py

--- build.py
+++ build.py
@@ -4,9 +4,12 @@ import tarfile
 
 user,repo = 'gohugoio','hugo'
 rels = urljson(f'https://api.github.com/repos/{user}/{repo}/releases/latest')
-dest = Path('hugo-download')
+dest = Path(f'{repo}-download')
 with urlopen(urlwrap(rels['tarball_url'])) as u:
     tarfile.open(mode='r:gz', fileobj=u).extractall(dest)
 
-dest.ls()[0].rename('hugo')
+dest.ls()[0].rename(repo)
 dest.rmdir()
+
+run(f'patch -l -p1 -d {repo} -i ../hugo.patch')
+run('go build --tags extended')
