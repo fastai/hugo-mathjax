5afc0150dcb15d563a20ed72a077c95bbbea8d55
Jeremy Howard <j@fast.ai>
2020-11-30 14:48:39 -0800

    build

---
+++ .github/scripts/build.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from fastcore.utils import *
+from ghapi import *
+import tarfile
+
+"""
+github_needs = {'prebuild': {'outputs': {'out': '{\n'
+                                 '  "step1": {\n'
+                                 '    "outputs": {\n'
+                                 '      "tag": "v0.79.0"\n'
+                                 '    },\n'
+                                 '    "outcome": "success",\n'
+                                 '    "conclusion": "success"\n'
+                                 '  }\n'
+                                 '}'},
+              'result': 'success'}}
+"""
+
+platform = dict(linux='linux', linux2='linux', win32='win', darwin='mac')[sys.platform]
+out = loads(nested_idx(github_needs, 'prebuild', 'outputs', 'out'))
+tag = nested_idx(out, 'step1', 'outputs', 'tag')
+
+rel = GhApi().repos.get_release_by_tag(owner='gohugoio', repo='hugo', tag=tag)
+print(rel.tarball_url)
+with urlopen(rel.tarball_url) as f: untar_dir(f, 'hugo')
+os.chdir('hugo')
+run(f'patch -l -p1 -i ../hugo.patch')
+run('go build')# --tags extended')
+ext_nm = 'hugo.exe' if platform=='win' else 'hugo'
+fn = f'hugo-{platform}.tgz'
+with tarfile.open(fn, "w:gz") as tar: tar.add(ext_nm)
+
+api = GhApi(owner='fastai', repo='hugo-mathjax', token=os.environ['GITHUB_TOKEN'])
+rel = api.repos.get_release_by_tag(tag)
+api.upload_file(rel, fn)
+
--- build.py
+++
@@ -1,9 +0,0 @@
-from fastcore.utils import *
-import os
-
-repo = 'hugo'
-rels = urljson(f'https://api.github.com/repos/gohugoio/{repo}/releases/latest')
-with urlopen(rels['tarball_url']) as f: untar_dir(f, repo)
-os.chdir(repo)
-run(f'patch -l -p1 -i ../hugo.patch')
-run('go build --tags extended')
